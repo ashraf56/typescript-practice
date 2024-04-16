ae7673207451908f1aa73981065723270eb9daa3
ashraf56 <ashrafulfahim07@gmail.com>
2024-04-16 15:32:44 +0600

    first commit

---
+++ output/style.js
@@ -0,0 +1,14 @@
+"use strict";
+function add(a, b) {
+    let c = a + b;
+    return c;
+}
+let sd = add(10, 32);
+let ob;
+ob = {
+    name: 'ashraful',
+    roll: 12,
+    regular: true
+};
+console.log(ob);
+// let c:string (Explicit )  let obj:(number | string )[] Union Types
---
+++ src/style.ts
@@ -0,0 +1,24 @@
+
+function add(a:number,b:number) {
+    let c = a+b
+return c
+}
+let sd= add(10,32)
+
+
+let ob:{
+    name:string,
+    roll:number,
+    regular:boolean
+}
+
+ob = {
+    name:'ashraful',
+    roll:12,
+    regular:true
+
+}
+console.log(ob);
+
+
+// let c:string (Explicit )  let obj:(number | string )[] Union Types
